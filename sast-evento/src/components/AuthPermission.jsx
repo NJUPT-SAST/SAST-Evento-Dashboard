129ba2b4225e5763005ab694f8b1ca6b9d844dd5
龙夫江 <2782522198@qq.com>
2023-08-28 11:17:54 +0800

    条件渲染组件修改后

--- sast-evento/src/components/AuthPermission.jsx
+++ sast-evento/src/components/AuthPermission.jsx
@@ -4,10 +4,15 @@ import React from "react";
 //根据权限进行条件渲染
 function AuthPermission({children}){
     const isPermisson=localStorage.getItem(children.type.name);
-    const Permisson=isPermisson.replace(isPermisson[0],isPermisson[0].toLowerCase())
-    return(
-        Permisson?<>{children}</>:Permisson
-    )
+    if(isPermisson==null){
+        return ""
+    }
+    else{
+        const Permisson=isPermisson.replace(children.type.name[0],children.type.name[0].toLowerCase())
+        return(
+            Permisson?<>{children}</>:""
+        )
+    }
 }
 
 export default AuthPermission;
